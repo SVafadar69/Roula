1a1f7b6c0c3a756608f308c402484f7c5fc8db2d
Rpyaanng <RyanPang12@gmail.com>
2023-03-08 17:01:10 -0700

    Added Tailwindcss

--- client/App.js
+++ client/App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-slate-300">Welcome To Roula!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
